683a4999c5183fe6d8ac3e046ab951051a0e4bf9
xmarsf <truong@vdx.vn>
2026-01-06 10:47:36 +0700

    [IMP] : update

--- ABasicCalculator/Integers/Task/src/lib.rs
+++ ABasicCalculator/Integers/Task/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn compute(a: u32, b: u32) -> u32 {
     // TODO: change the line below to fix the compiler error and make the tests pass.
-    let multiplier: /*TODO*/ = 4;
+    let multiplier: u32 = 4;
     a + b * multiplier
 }
