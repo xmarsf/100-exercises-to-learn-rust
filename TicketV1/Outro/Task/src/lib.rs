ea0f95a37784dabeb22da77ef92126ce421c8d0f
truongpx <xtruongptit@gmail.com>
2026-01-12 22:00:52 +0700

    [IMP] : update

--- TicketV1/Outro/Task/src/lib.rs
+++ TicketV1/Outro/Task/src/lib.rs
@@ -12,4 +12,68 @@
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
 
-/* TODO */
+pub struct Order {
+    product_name: String,
+    quantity: i32,
+    unit_price: i32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: i32, unit_price: i32) -> Self {
+        _check_product_name(&product_name);
+        _check_quantity(quantity);
+        _check_unit_price(unit_price);
+        Self {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+    pub fn total(&self) -> i32 {
+        self.quantity * self.unit_price
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &i32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &i32 {
+        &self.unit_price
+    }
+
+    pub fn set_quantity(&mut self, quantity: i32) {
+        _check_quantity(quantity);
+        self.quantity = quantity;
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        _check_product_name(&product_name);
+        self.product_name = product_name;
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: i32) {
+        _check_unit_price(unit_price);
+        self.unit_price = unit_price;
+    }
+}
+fn _check_product_name(name: &String) {
+    if name.is_empty() || name.bytes().len() > 300 {
+        panic!("Ticket name is too long!");
+    }
+}
+
+fn _check_quantity(quantity: i32) {
+    if quantity <= 0 {
+        panic!("Quantity is too small!");
+    }
+}
+
+fn _check_unit_price(unit_price: i32) {
+    if unit_price <= 0 {
+        panic!("UnitPrice is too small!");
+    }
+}
